77e430fb38a2408a43512983d06e7640ab6a02f6
TerkaSm <framboiseart@email.cz>
2024-07-25 20:55:31 +0200

    flashcards

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,5 +1,13 @@
 "use server";
+import { generateText } from 'ai';
+import {openai} from '@ai-sdk/openai';
 
-export async function generateFlashcards() {
-  // Use AI ;-)
+export async function generateFlashcards(userPrompt: string, language: string) {
+  const {text} = await generateText ({
+    system:`You are a language teacher. Your goal is to generate a list of words in a given area and in ${language} which I will give you. Please separate each word with a comma. YOU MUST  GENERATE ONLY UP TO 10 WORDS.`,
+    prompt: userPrompt, 
+    model: openai("gpt-4o-mini")
+  });
+
+  return text;
 }
